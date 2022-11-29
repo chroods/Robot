b0942abbbb036d8ffaaee85cc1638b6e3ce39b69
David Wanderson <daviddonnna@hotmail.com>
2022-11-28 22:18:24 -0300

    update projeto

---
+++ src/Pages/Start/index.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { View, ScrollView, Text, Image, StyleSheet } from "react-native";
+
+export default function Start(){
+    return (
+        <View style={styles.container}>
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View style={{alignItems: "center"}}>
+                    <Image source={require("../../assets/icons/logo3.png")}
+                    style={styles.logo} />
+                    <Text style={styles.descrition}>
+                        Vamos Transforma sua vida {"/n"} em jogo, buscando sempre o melhor jogo
+                    </Text>
+                </View>
+            </ScrollView>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor: "rgba(21,21,21,0.98)",
+    },
+
+    logo:{
+        width: 300,
+        height: 60,
+        marginTop: 60,
+        marginBottom: 20,
+    },
+
+    descrition:{
+        color: "#ffffff",
+        fontSize: 20,
+        textAlign: "center",
+        marginVertical: 60,
+    }
+})
